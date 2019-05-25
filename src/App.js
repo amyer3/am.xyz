a9c1af599f0fa8505894ff69b2fc92dee85c2f5b
amyer3 <aj.myers93@gmail.com>
2019-05-24 21:53:03 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
 import React from 'react';
 import './App.css';
+import Links from './links/Links'
+import Logo from './logo/Logo'
+import './modeSlider.css'
+import './langSlider.css'
+
+class App extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            mode: 'day',
+            lang: 'en'
+        }
+    }
+
+    componentDidMount(){};
+
+    changeMode = () => {
+        if (this.state.mode === 'day' || ""){
+            this.setState({mode: "night"});
+            document.body.style = "background-color: black;"
+        }
+        else if (this.state.mode === 'night'){
+            this.setState({mode: "day"});
+            document.body.style = "background-color: white;"
+        }
+    }
+    changeLang = () => {
+        if (this.state.lang === 'en' || ""){
+            this.setState({lang: "fr"});
+        }
+        else if (this.state.lang === 'fr'){
+            this.setState({lang: "en"});
+        }
+    }
+
+    render() {
 
 
-function App() {
-    return (
-        <div className="App">
-            <div className='container'>
-                <div className="core nom a"><p>a</p></div>
-                <div className="core nom l"><p>l</p></div>
-                <div className="core nom e"><p>e</p></div>
-                <div className="inverse nom x"><p>x</p></div>
+        return (
+
+            <div className={this.state.mode === "day" ? "inverse pageMax app" : "core pageMax app"}>
+                <div className='header'>
+                   <div className='inline logo'>
+                    <Logo mode={this.state.mode}/>
+                </div>
+
+                <Links className = 'link inline' mode={this.state.mode}/>
+
+                    <div className='toggle'>
+                        <label className="switch">
+                            <input type="checkbox" onChange={this.changeMode}/>
+                            <span className="slider round"> </span>
+                        </label>
+                        <label className="langSwitch">
+                            <input type="checkbox" onChange={this.changeLang}/>
+                            <span className="langSlider round"> </span>
+                        </label>
+                    </div>
+
+
+                </div>
+
+
+
             </div>
-            <button onClick={() => changeMode}>CHANGE</button>
-
-        </div>
-    );
+        )
+            ;
+    }
 }
 
 export default App;
