71802e238c24f271a8d102844da6961ec1b2bb4a
amyer3 <aj.myers93@gmail.com>
2019-07-27 20:35:53 -0700

    added words, state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './langSlider.css'
-import paperplane from './assets/icons/new.svg'
-import gh from './assets/icons/icons8-github.svg'
-import li from './assets/icons/icons8-linkedin.svg'
-import roles from './assets/roles.json';
-import work from './assets/work.json'
-import gb from './assets/icons/gb-flag.svg'
-import fr from './assets/icons/fr-flag.svg'
 import styled from 'styled-components'
+import cut from './cut.svg'
 
-const en_prename = `Hi, I'm `
-const fr_premane = `Salut, je m'appelle `
-const en_prebio = "And I'm a"
-const fr_prebio = "Et je suis un"
-const en_contact ="Contact me"
-const fr_contact = "Contactez"
-const en_travel=" Where I've Been "
-const fr_travel = "Mes Voyages"
-const en_work = "What I've Done"
-const fr_work="Mes Travailles"
-
+/*
+Use this to pull all photos from a folder, and make an array from them
 function importAll(r) {
     return r.keys().map(r);
 }
 const images = importAll(require.context('./assets/photos/', false, /\.(png|jpe?g|svg)$/));
+*/
 
-export function App(){
-    const [en, changeLang] = useState(0)
+const BackgroundDiv = styled.div`
+    background-color: #0D1836;
+    width: 100vw;
+    height: 100vh;
+`
+const CutImage = styled.img`
+    height: 100vh;
+    width: auto;
+    float: right;
+`
+const LangToggle = styled.p`
+    color: white;
+    font-family: 'ABeeZee', Serif;
+    padding: 0;
+    width: 10vw;
+    text-align: left;
+    grid-area: toggle;
+    cursor: pointer;
+`
 
+const LangP = styled.p`
+    font-size: ${props => props.size}pt;
+    padding: 0;
+    margin: 0;
+`
+
+const Words = styled.div`
+    font-size: 22pt;
+    line-height: 100%;
+    padding: 0;
+    width: 50vw;
+    display: inline-block;
+    grid-area: words;
+`
+
+const InlinePara = styled.p`
+    font-size: 42pt;
+    padding: 0;
+    color: white;
+    font-family: 'ABeeZee', Serif;
+`
+const Grid = styled.div`
+    display: inline-grid;
+    grid-template-columns: 10vw 40vw ;
+    grid-template-rows: 10vh 15vh 55vh 20vh;
+    grid-template-areas: 
+    "cs toggle"
+    "cs space"
+    "cs words"
+    "cs buttons"
+`
+const LinkButton = styled.button`
+    border: 3px solid white;
+    border-radius: 5px;
+    display: inline-block;
+    color: white;
+    background-color: transparent;
+    max-width: 200px;
+    max-height: 50px;
+    font-size: 32px;
+    font-family: 'ABeeZee', Serif;
+    grid-area: ${props => props.area}
+`
+const ButtonGrid = styled.div`
+    grid-template-columns: 30% 5% 30% 5% 30%
+    grid-template-rows: 50% 50%;
+    grid-template-areas:
+    "li s1 gh s2 ct"
+    "map map map map map";
+    grid-area: buttons;
+    margin: 0;
+    padding: 0;
+`
+
+export function App() {
+    const [en, changeLang] = useState(1)
+    /*
     changeLang = () => {
         this.state.lang === 'en' || "" ? this.setState({lang: "fr"}) : this.setState({lang: "en"});
     };
@@ -36,11 +96,29 @@ export function App(){
         console.log(this.state.mapVisible)
         this.state.mapVisible === 0 ? this.setState({mapVisible: 1}) : this.setState({mapVisible: 0})
     }
-    
-    return(
-        <div>
+    */
+    var handleChange = () => changeLang(!en) && console.log(en)
 
-        </div>
+    return (
+        <BackgroundDiv>
+            <Grid>
+                <LangToggle onClick={handleChange}>
+                    <LangP size={en ? 14 : 12}>EN</LangP> | <LangP size={en ? 12 : 14}>FR</LangP>
+                </LangToggle>
+                <Words>
+                    <InlinePara><u>Alex</u></InlinePara>
+                    <InlinePara>{en ? "Engineering" : "Ingénierie"}.</InlinePara>
+                    <InlinePara>{en ? "Strategy" : "Stratégie"}.</InlinePara>
+                    <InlinePara>{en ? "Mostly edible baking" : ""}.</InlinePara>
+                </Words>
+                <ButtonGrid>
+                    <LinkButton a={'linkedin.com/in/alexjmyers'} area={'li'}>Linkedin</LinkButton>
+                    <LinkButton a={'linkedin.com/in/alexjmyers'} area={'gh'}>Github</LinkButton>
+                    <LinkButton a={'mailto:me@am.xyz?'} area={'ct'}>Contact</LinkButton>
+                </ButtonGrid>
+            </Grid>
+            <CutImage src={cut} />
+        </BackgroundDiv>
     )
 }
 
