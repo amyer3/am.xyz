aa9d03d8b653755c32a9bb3bde6c9995f26789ea
amyer3 <aj.myers93@gmail.com>
2019-08-05 13:27:09 -0500

    adjusting wa button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import {Router, Link, Route, BrowserRouter} from 'react-router-dom'
 
-const Home = React.lazy(import('./views/Home'))
-const Photo = React.lazy(import('./views/Photography'))
+const Home = React.lazy(() => import('./views/Home'))
+const Photo = React.lazy(() => import('./views/Photography'))
 
 ReactDOM.render(
 <BrowserRouter>
-    <React.Suspense>
+    <React.Suspense fallback={<div></div>}>
         <Route path='/p' name='Photography' render={props => <Photo {...props}/>} />
         <Route exact path='/' name='Home' render={props => <Home {...props}/>} />
     </React.Suspense>
