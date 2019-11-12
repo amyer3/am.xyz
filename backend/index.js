74dfa065812082600189c4b11f5da703e9f3e2c8
amyer3 <aj.myers93@gmail.com>
2019-11-11 20:57:12 -0800

    *added in express static routing

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan')
+const path = require('path')
 
 
 const logs = require("./routes/logs.routes")
@@ -20,8 +21,10 @@ app.use(logger('dev'))
  * app uses /api/prefix then routes to images to find specific resource requested
  * @example /api/i/count => api/i routes to images, then images calls /count to return data
  */
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use('/api/i', images)
 app.use('/api/l', logs)
 app.use('/api/m', maps)
 app.use('/resource/', express.static(__dirname+'/public'))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
