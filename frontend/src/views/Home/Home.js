49315ac43d82e65f93609062be15590d2b8a3136
alex <alex@Alexanders-MacBook-Pro.local>
2020-12-31 19:24:50 -0800

    Pre eject

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -18,12 +18,12 @@ export default function Home() {
     return (
         <div className='bg-div'>
             <div className="container-fluid abz">
-                <div className="col-sm-4">
+                <div className="flex flex-col sm:w-4/12">
                     <img className="h-100 cut-img d-none d-md-block p-0" src={cut} />
                     <img className="cut-img d-block d-md-none p-0" src={mcut} />
                 </div>
                 <div className="row">
-                    <div className="col-sm-8 text-light">
+                    <div className="flex flex-col sm:w-8/12 text-light">
                         <div className="row mb-4 pl-3 pt-3">
                             <a onClick={handleChange} className="align-middle point">
                                 <p className={language == "en" ? "" : "small"}>EN</p>
@@ -52,7 +52,7 @@ export default function Home() {
                         </div>
 
                         <div className="row mb-4">
-                            <div className="col-6 col-md-3 d-flex justify-content-center text-center">
+                            <div className="flex flex-col w-6/12flex flex-col md:w-3/12 d-flex justify-content-center text-center">
                                 <a href={"https://www.linkedin.com/in/alexjmyers/"}
                                     className="btn btn-xl btn-block btn-outline-light border-10"
                                 >
@@ -61,7 +61,7 @@ export default function Home() {
                                 </a>
                             </div>
 
-                            <div className="col-6 col-md-3 d-flex justify-content-center text-center">
+                            <div className="flex flex-col w-6/12flex flex-col md:w-3/12 d-flex justify-content-center text-center">
                                 <a href={"https://github.com/amyer3"}
                                     className="btn btn-xl btn-block btn-outline-light border-10"
                                 >
@@ -71,7 +71,7 @@ export default function Home() {
                                 </a>
                             </div>
 
-                            <div className="col-6 col-md-3 d-flex justify-content-center text-center m-pad">
+                            <div className="flex flex-col w-6/12flex flex-col md:w-3/12 d-flex justify-content-center text-center m-pad">
                                 <a className="btn btn-xl btn-block btn-outline-light border-10"
                                     href={`mailto:${process.env.REACT_APP_EMAIL}`}
                                     area={"ct"}
@@ -81,7 +81,7 @@ export default function Home() {
                                 </a>
                             </div>
 
-                            <div className="col-6 col-md-3 d-sm-flex d-md-none justify-content-center text-center m-pad">
+                            <div className="flex flex-col w-6/12flex flex-col md:w-3/12 d-sm-flex d-md-none justify-content-center text-center m-pad">
                                 <a href="/p" className="btn btn-xl btn-block btn-outline-light border-10">
                                     <img className="d-sm-block d-md-none" src={cam} className="d-md-none" />
                                 </a>
@@ -89,18 +89,18 @@ export default function Home() {
                         </div>
 
                         <div className="row mt-4 d-md-flex justify-content-center text-center d-none">
-                            <div className="col-6 justify-content-center">
+                            <div className="flex flex-col w-6/12justify-content-center">
                                 <a href="/photo" className="btn btn-xl btn-block btn-outline-light border-10">
                                     <h2 className="mb-0 d-none d-md-block">Photography</h2>
                                 </a>
                             </div>
 
-                            <div className="col-sm-3 d-flex justify-content-center text-center d-none d-md-block">
+                            <div className="flex flex-col sm:w-3/12 d-flex justify-content-center text-center d-none d-md-block">
                                 <a href="/data" className="btn btn-xl btn-block btn-outline-light border-10">
                                     <h2 className="mb-0 d-none d-md-block">Data</h2>
                                 </a>
                             </div>
-                            <div className="col-sm-3" />
+                            <div className="flex flex-col sm:w-3/12" />
                         </div>
                     </div>
 
